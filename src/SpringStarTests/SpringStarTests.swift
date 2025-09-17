ed00f80169551976929163db7224d95fe2bf825c
Nayeli Villa <villan@merrimack.edu>
2025-09-17 19:49:41 -0400

    Build: Added base template Apple Vision Pro app

---
+++ src/SpringStarTests/SpringStarTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpringStarTests.swift
+//  SpringStarTests
+//
+//  Created by Jelly on 9/17/25.
+//
+
+import Testing
+@testable import SpringStar
+
+struct SpringStarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
